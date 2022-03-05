83f3b02fff062b6c3f430eaa9f2f08736203050a
Anel Arciniega Oliver <53792546+DarkAzuka@users.noreply.github.com>
2022-03-05 03:05:12 +0000

    add mode end game

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+from replit import clear
 import random
-
 
 print('''
               _                              
@@ -84,58 +84,58 @@ bulbasur = ('''
 ''')
 
 images = [pikachu, squirtle, bulbasur]
-vida_usuario =int(80) 
+vida_usuario = int(80)
 vida_pc = int(80)
 
-#elegir pokemon a base de una lista 
+# elegir pokemon a base de una lista
 usuario_pokemon = int(input("Que pokemon tu eligues: [0] Pikachu, [1] Squirtle, [2] Bulbasur.\n"))
-#print(images[usuario_pokemon])
-#Codigo batalla
-#pokemon de pc
+
 pc = random.randint(0, 2)
 print(f"Pc:{images[pc]}")
 
-#turno Pc
-#while vida_pc > 0 and vida_usuario > 0:
-#ataque_pc = random.randint(1, 2)
 def rand_dmg(hp):
     min_dmg = 1
-    try: max_dmg = int(hp / 2)
-    except: return 0
+    try:
+        max_dmg = int(hp / 2)
+    except:
+        return 0
 
-    try: return random.randint(min_dmg, max_dmg)
-    except: return min_dmg
+    try:
+        return random.randint(min_dmg, max_dmg)
+    except:
+        return min_dmg
+#mientras no se termine el juego sigue el bucle
+end_game = False
+while end_game == False:
+    attacks_count = 0
+    while vida_pc > 0 and vida_usuario > 0:
+        if attacks_count % 2:
+            vida_pc -= rand_dmg(hp=vida_pc)
+            print("Te atacaron con Placaje!\n")
+        else:
+            vida_usuario -= rand_dmg(hp=vida_usuario)
+            print("Te atacaron con Araniazo!\n")
 
-attacks_count = 0
-while vida_pc > 0 and vida_usuario > 0:
-
-    if attacks_count % 2:
-        vida_pc -= rand_dmg(hp=vida_pc)
-        print("Te atacaron con Placaje!\n")
-    else:
-        vida_usuario -= rand_dmg(hp=vida_usuario)
-        print("Te atacaron con Araniazo!\n")
-
-    attacks_count += 1
-    print("vida_pc", vida_pc)
-    print("vida_usuario", vida_usuario)
-    attack_usuario = None
-    while attack_usuario != "P" and attack_usuario != "A":
-      print('Tu turno:')
-      print(images[usuario_pokemon])
-      attack_usuario = input("Que ataque quieres usar [A]araniazo, [P] placaje ?")
-      if attack_usuario == "A":
-        print(f"Usas Araniazo!")
-        vida_pc -= 10
-        print(f"la vida de Pc es: {vida_pc}, Tu vida es:{vida_usuario}\n")
-      elif attack_usuario == "P":
-        print(f"Usas Placaje!")
-        vida_pc -= 12
-        print(f"la vida de Pc es: {vida_pc}, Tu vida es {vida_usuario}\n")
-        if vida_usuario <=  0:
-          print('Pc Gana !!!\n')
+        attacks_count += 1
+        print("vida_pc", vida_pc)
+        print("vida_usuario", vida_usuario)
+        attack_usuario = None
+        #turno usuario
+        while attack_usuario != "P" and attack_usuario != "A":
+            print('Tu turno:')
+            print(images[usuario_pokemon])
+            attack_usuario = input("Que ataque quieres usar [A]araniazo, [P] placaje ?")
+            if attack_usuario == "A":
+                print(f"Usas Araniazo!")
+                vida_pc -= 10
+                print(f"la vida de Pc es: {vida_pc}, Tu vida es:{vida_usuario}\n")
+            elif attack_usuario == "P":
+                print(f"Usas Placaje!")
+                vida_pc -= 12
+                print(f"la vida de Pc es: {vida_pc}, Tu vida es {vida_usuario}\n")
+        if vida_usuario <= 0:
+            print('Pc Gana !!!\n')
+            end_game == True
         elif vida_pc <= 0:
-          print("YOU WIN !!")
-
-
-
+            print("YOU WIN !!")
+            end_game == True
